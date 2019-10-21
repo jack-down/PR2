567cabe40d536b459d7abbda8680325961411061
sergio steven <stevenst99@hotmail.com>
2019-10-20 22:47:49 -0500

    Inicio proyecto

---
+++ Perrinder/app/src/main/java/com/example/perrinder/ConfrimacionCorreo.java
@@ -0,0 +1,14 @@
+package com.example.perrinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfrimacionCorreo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confrimacion_correo);
+    }
+}
